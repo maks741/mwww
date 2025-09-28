876409b1a1fc7d6edfe0109a655bb6c1e23a74bb
Maksym Kozak <bersercer70@gmail.com>
2025-09-28 14:35:06 +0300

    fix bug with reading loop closing after 1 command

--- src/main/java/org/maks/musicplayer/service/FifoService.java
+++ src/main/java/org/maks/musicplayer/service/FifoService.java
@@ -2,22 +2,27 @@ package org.maks.musicplayer.service;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.Paths;
 
 public class FifoService {
     public void read() {
-        String fifoPathStr = Paths.get("commands.fifo").toString();
+        String fifoFileName = "commands.fifo";
+        String fifoPathStr = Paths.get(fifoFileName).toString();
 
         new Thread(() -> {
-            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fifoPathStr))) {
+            try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fifoPathStr));
+                 OutputStream holdFifoOpenStream = new FileOutputStream(fifoPathStr)) {
+
                 String line;
                 while ((line = bufferedReader.readLine()) != null) {
                     System.out.println("LINE: " + line);
                 }
             } catch (FileNotFoundException e) {
-                throw new RuntimeException("commands.fifo not found. Run 'mkfifo commands.fifo' in the root project dir");
+                throw new RuntimeException(fifoFileName + " not found. Run 'mkfifo " + fifoFileName + "' in the root project dir");
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
