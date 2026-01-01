80456d1d5bac1a856713f2c63dc332a0e3d35f23
Maksym Kozak <bersercer70@gmail.com>
2026-01-01 18:07:38 +0200

    feat: update keybindings

--- src/main/java/org/maks/mwww_daemon/controller/Widget.java
+++ src/main/java/org/maks/mwww_daemon/controller/Widget.java
@@ -67,8 +67,8 @@ public class Widget implements Initializable, FifoCommandSubscriber {
     }
 
     private void addKeybindings() {
-        KeyCombination next = new KeyCodeCombination(KeyCode.RIGHT, KeyCombination.ALT_DOWN);
-        KeyCombination previous = new KeyCodeCombination(KeyCode.LEFT, KeyCombination.ALT_DOWN);
+        KeyCombination next = new KeyCodeCombination(KeyCode.RIGHT, KeyCombination.CONTROL_DOWN);
+        KeyCombination previous = new KeyCodeCombination(KeyCode.LEFT, KeyCombination.CONTROL_DOWN);
         KeyCombination skipForward = new KeyCodeCombination(KeyCode.RIGHT, KeyCombination.SHIFT_DOWN);
         KeyCombination skipBackward = new KeyCodeCombination(KeyCode.LEFT, KeyCombination.SHIFT_DOWN);
         KeyCombination toggleRepeat = new KeyCodeCombination(KeyCode.R);
@@ -105,7 +105,7 @@ public class Widget implements Initializable, FifoCommandSubscriber {
             });
 
             scene.setOnKeyReleased(keyEvent -> {
-                if (KeyCode.ALT == keyEvent.getCode()) {
+                if (KeyCode.CONTROL == keyEvent.getCode()) {
                     playerService.play();
                 }
             });
